966cf69078a139cb8ae1f39c5651363184b712d7
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-07-14 21:32:15 -0300

    test: add basic tests

---
+++ spec/card_spec.rb
@@ -0,0 +1,30 @@
+class Card
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+
+RSpec.describe Card do
+  # before do
+  #   @card = Card.new("Ace", "Spades")
+  # end
+
+  let(:card) { Card.new("Ace", "Spades") }
+
+  it "has a rank" do
+    puts methods
+    expect(card.rank).to eq("Ace")
+  end
+
+  it "has a suit" do
+    expect(card.suit).to eq("Spades")
+  end
+
+  it "has a custom error message" do
+    comparison = "Spades"
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
+  end
+end
